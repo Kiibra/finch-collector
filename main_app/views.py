1800090454665d7874bf81517ea5dcc220d43af4
kiibra <kibra99@gmail.com>
2024-05-28 16:36:55 -0400

    about finchCollection route and view function

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello Finches🦜</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About the FinchCollector🦜</h1>')
