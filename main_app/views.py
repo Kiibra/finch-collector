3c44b43c80cf830c86679102c011d16fb22bc5c9
kiibra <kibra99@gmail.com>
2024-05-28 16:32:02 -0400

    define main app's home page URL and views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+# Add the following import
+from django.http import HttpResponse
 
-# Create your views here.
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello Finches🦜</h1>')
