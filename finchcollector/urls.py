c7be42cda9d8f1d8eebf88892899e94ecf65d2ec
kiibra <kibra99@gmail.com>
2024-05-28 16:27:30 -0400

    define routes & URL setup

--- finchcollector/urls.py
+++ finchcollector/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+# Add the include function to the import
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # In this case '' represents the root route
+    path('', include('main_app.urls')),
 ]
