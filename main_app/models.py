3ee3f0354a123484e8aab68919482d94be995314
kiibra <kibra99@gmail.com>
2024-05-29 11:49:51 -0400

    complete performing CRUD using ORM

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Cat(models.Model):
+class Finch(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
