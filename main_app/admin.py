d236eb42618e9762ae9d83bd61256f07aeae6b12
kiibra <kibra99@gmail.com>
2024-05-30 12:28:04 -0400

    complete making & running migration and register Feeding in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 # import your models here
-from .models import Finch
+# add Feeding to the import
+from .models import Finch, Feeding
 
 # Register your models here
 admin.site.register(Finch)
+# register the new Feeding Model 
+admin.site.register(Feeding)
