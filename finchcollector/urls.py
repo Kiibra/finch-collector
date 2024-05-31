d7f49d4e09d731769b16545dd327c3f58c0fff15
kiibra <kibra99@gmail.com>
2024-05-31 13:45:12 -0400

    add URLs for authentication

--- finchcollector/urls.py
+++ finchcollector/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # In this case '' represents the root route
     path('', include('main_app.urls')),
+    # include the built-in auth urls for the built-in views
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
