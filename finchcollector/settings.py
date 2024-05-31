04f1666e9b7df93d67f6b8cf6458142379929a27
kiibra <kibra99@gmail.com>
2024-05-31 13:53:26 -0400

    add default redirect after logging in

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Add this variable to specify where successful logins should redirect to
+LOGIN_REDIRECT_URL = 'finch-index'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
