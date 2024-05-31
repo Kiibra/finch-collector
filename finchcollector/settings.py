900dffd569a2ea7865a8fdb4e71f66095b44e0d7
kiibra <kibra99@gmail.com>
2024-05-31 13:58:34 -0400

    specify where logout redirects to

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -129,6 +129,9 @@ STATIC_URL = 'static/'
 # Add this variable to specify where successful logins should redirect to
 LOGIN_REDIRECT_URL = 'finch-index'
 
+# Add this variable to specify where logging out redirects to
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
